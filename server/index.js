da740de35675f89a9a07cab13d646d401cbb19c0
siddhu458 <siddhuchaparthi86@gmail.com>
2025-04-08 17:02:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-const serverless = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -8,7 +7,7 @@ const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
+const productRoutes = require('./routes/productRoutes'); 
 const utilityRoutes = require('./routes/utilityRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
@@ -16,29 +15,21 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-app.use(cors({
-  origin: ["https://ai-customer-chat-clean-t9xt.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
-
 
 
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/products', productRoutes);
+app.use('/api/products', productRoutes); 
 app.use('/api/utilities', utilityRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/backup', express.static(path.join(__dirname, '../backup')));
+app.use('/backup', express.static(path.join(__dirname, 'backup')));
 
-module.exports = app;
-module.exports.handler = serverless(app);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🟢 Server running on http://localhost:${PORT}`);
+});

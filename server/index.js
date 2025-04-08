1ee9ea9034d9b30795c0a40f00b22ae8ef808ab8
siddhu458 <siddhuchaparthi86@gmail.com>
2025-04-08 13:32:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
+const serverless = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const connectDB = require('../config/db');
 const path = require('path');
 
-const authRoutes = require('./routes/authRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes'); 
-const utilityRoutes = require('./routes/utilityRoutes');
-const adminRoutes = require('./routes/adminRoutes');
+
+const authRoutes = require('../routes/authRoutes');
+const chatRoutes = require('../routes/chatRoutes');
+const userRoutes = require('../routes/userRoutes');
+const productRoutes = require('../routes/productRoutes');
+const utilityRoutes = require('../routes/utilityRoutes');
+const adminRoutes = require('../routes/adminRoutes');
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
 app.use(cors({
   origin: ["https://ai-customer-chat-clean-t9xt.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -27,14 +29,11 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/products', productRoutes); 
+app.use('/api/products', productRoutes);
 app.use('/api/utilities', utilityRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/backup', express.static(path.join(__dirname, 'backup')));
+app.use('/backup', express.static(path.join(__dirname, '../backup')));
 
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🟢 Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
+module.exports.handler = serverless(app);

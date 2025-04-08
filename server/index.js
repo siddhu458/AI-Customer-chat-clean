c0e9e646275c1e2ed33a4031d10e011713e53f72
siddhu458 <siddhuchaparthi86@gmail.com>
2025-04-08 14:34:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: "Welcome to the AI Customer Chat Server!" });
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 
